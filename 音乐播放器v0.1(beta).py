48c9ee76d14ec5eae700ca91a9b822f25b0607e6
xhdndmm <zhangzx1149@outlook.com>
2024-02-24 08:09:45 +0800

    Changes to be committed: 	new file:   "\351\237\263\344\271\220\346\222\255\346\224\276\345\231\250v0.1(beta).py"

---
+++ 音乐播放器v0.1(beta).py
@@ -0,0 +1,20 @@
+print("音乐播放器")
+print("v0.1(beta)")
+import time
+import pygame
+import tkinter as tk
+from tkinter import filedialog
+import librosa
+root = tk.Tk()
+root.withdraw()
+while True:
+    file = filedialog.askopenfilename()
+    duration = librosa.get_duration(filename=file)
+    pygame.mixer.init()
+    print('正在播放',file)
+    pygame.mixer.music.load(file)
+    pygame.mixer.music.play()
+    time.sleep(duration)
+    pygame.mixer.music.stop()
+    print("播放完毕 请选择其他文件播放")
+    time.sleep(3)
