c710058e11dc6523541e4c21a00267280cd9c3a6
xhdndmm <zhangzx1149@outlook.com>
2024-02-26 18:11:26 +0800

    Changes to be committed: 	modified:   "\344\270\213\350\275\275\345\231\250v0.1.py"

--- 下载器v0.1.py
+++ 下载器v0.1.py
@@ -1,8 +1,11 @@
 #开发环境：Ubuntu 22.04.4 LTS python3.10.12
 import wget
-url = input("下载链接？")
-where = input("下载到哪个路径？")
-try:
-    wget.download(url,where)
-except Exception as e:
-    print(e)
+from time import sleep
+while True :
+    try:
+        url = input("下载链接？")
+        where = input("下载到哪个路径？")
+        wget.download(url,where)
+    except Exception as e:
+        print(e)
+        sleep(3)
