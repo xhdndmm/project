fcdb09fb35b0ab48a29ec7a4b684fdd71b3885f2
xhdndmm <zhangzx1149@outlook.com>
2024-02-26 15:25:40 +0800

    要提交的变更： 	修改：     music player v0.1.py

--- music player v0.1.py
+++ music player v0.1.py
@@ -1,4 +1,6 @@
-#目前已知BUG：似乎无法在linux下运行（测试环境：Ubuntu 22.04.4 LTS)
+#目前已知BUG
+#似乎无法在linux下运行（测试环境：Ubuntu 22.04.4 LTS python3.10.12)
+#似乎播放完音乐不能再次选择音乐（虽然程序在运行但似乎没有任何反应 作者尝试注释掉第17行代码 可以正常选择 但这行代码必须有 不然无法播放）（此BUG测试环境为WIN10 22H2 python3.10.8)
 print("music player")
 print("v0.1")
 import time
