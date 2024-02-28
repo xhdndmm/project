d64f7525a5f6311782999b4402f0b2e11c72c0fd
xhdndmm <zhangzx1149@outlook.com>
2024-02-28 10:26:02 +0800

    要提交的变更： 	修改：     "\344\270\213\350\275\275\345\231\250v0.2.py" 	修改：     "\344\270\213\350\275\275\345\231\250v0.3.py"

--- 下载器v0.3.py
+++ 下载器v0.3.py
@@ -1,6 +1,7 @@
 #已知小问题：似乎此程序会先下载到根程序一个文件夹的地方 再移动到指定路径 且如果下载中途关闭程序 那么缓存还会保存
 print("下载器")
 print("v0.3")
+print("此版本增加了自动检查故障原因这个功能")
 from wget import download
 from time import sleep
 import urllib.request
