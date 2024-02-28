bc9ae1ba0f0eed84493f8f6f11961e6f9fc68e63
xhdndmm <zhangzx1149@outlook.com>
2024-02-28 20:05:40 +0800

    Changes to be committed: 	new file:   "\344\270\213\350\275\275\345\231\250v0.01.py"

---
+++ 下载器v0.01.py
@@ -0,0 +1,10 @@
+print("下载器 v0.01")
+from wget import download
+while True :
+    try:
+        url = input("链接")
+        where = input("路径")
+        download(url,where)
+    except Exception as e:
+        print(e)
+    break
