c7b85477fb185cfd855b32524d56c1326df6a00a
xhdndmm <zhangzx1149@outlook.com>
2024-03-31 13:29:12 +0800

    Changes to be committed: 	modified:   "\350\247\206\351\242\221\351\207\207\351\233\206\345\231\250/\350\247\206\351\242\221\351\207\207\351\233\206\345\231\250v0.1.py"

--- 视频采集器/视频采集器v0.1.py
+++ 视频采集器/视频采集器v0.1.py
@@ -1,5 +1,5 @@
 print("视频采集器v0.1")
-print("可以按q来退出")
+print("按q来退出")
 
 #⣿⣿⣿⠟⠛⠛⠻⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡟⢋⣩⣉⢻
 #⣿⣿⣿⠀⣿⣶⣕⣈⠹⠿⠿⠿⠿⠟⠛⣛⢋⣰⠣⣿⣿⠀⣿
@@ -19,7 +19,7 @@ cap = cv2.VideoCapture(0)
 while True:
     ret, frame = cap.read()
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    cv2.imshow("视频采集器v0.1", frame)
+    cv2.imshow("Frame", frame)
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 cv2.destroyAllWindows()
